538ce48c76578123bd1dff41e243319003dee6e7
Jan Vargovský <jan.vargovsky@gmail.com>
2017-08-23 21:39:06 +0200

    Adblock alert is not visible on the donate page. Donate page text. Script protocol fixes.

--- PoGo.Web/wwwroot/js/site.es5.js
+++ PoGo.Web/wwwroot/js/site.es5.js
@@ -4,7 +4,7 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip();
 
     setTimeout(function () {
-        if (typeof window.qs === 'undefined') $("#adblockAlert").removeAttr("hidden");
+        if (typeof window.qs === 'undefined' && $(".ads").length != 0) $("#adblockAlert").removeAttr("hidden");
     }, 2000);
 });
 
